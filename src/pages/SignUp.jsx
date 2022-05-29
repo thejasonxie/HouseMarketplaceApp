0c03c63862e9a84308e9fbf1f6ac999d42a05f35
Jason Xie <thejasonxie@gmail.com>
2022-05-28 23:06:48 -0400

    Implemented user registration to firebase

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
-// import { toast } from 'react-toastify'
-// import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
-// import OAuth from '../components/OAuth'
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { db } from '../firebase.config'
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false)
@@ -25,6 +25,29 @@ function SignUp() {
     }))
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+      const user = userCredential.user
+
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+
+      const formDataCopy = {...formData}
+      delete formDataCopy.password
+      formDataCopy.timestamp = serverTimestamp()
+
+      await setDoc(doc(db, "users", user.uid), formDataCopy)
+
+      navigate("/")
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
       <div className="pageContainer">
@@ -32,7 +55,7 @@ function SignUp() {
           <p className="pageHeader">Welcome Back!</p>
         </header>
         
-        <form>
+        <form onSubmit={onSubmit}>
           <input 
             type="text" 
             className="nameInput" 
